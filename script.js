2af0974ce3563875360cbebc29319fbb3764e355
Sh1Tt <info@mauricestolk.nl>
2022-08-17 22:29:09 +0200

    Testing

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function handleKeyDown() {
-  if (event.key === 'Enter') {
-    location.href = "https://impervious.domains/name/" + event.target.value + ".contract/register/";
-  }
+const settings = require("/settings.json");
+
+const reWriteTld = tld => {
+  const allElements = Array.from(document.querySelectorAll());
+  allElements.forEach(element => {
+    console.log(element.innerText);
+  })
 };
+
+const handleKeyDown = e => {
+  if (e.key === 'Enter') {
+    const url = `https://impervious.domains/name/${e.target.value}${yourtld}/register/`;
+    location.href = url;
+  };
+};
+
+const loadedHandler = () => {
+  reWriteTld(settings.yourtld);
+};
+
+(() => {
+  document.addEventListener("DOMContentLoaded", loadedHandler, false);
+})();
