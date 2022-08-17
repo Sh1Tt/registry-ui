3fd838be8ec25ec66f25f7b7cda7daecacf2898b
Sh1Tt <info@mauricestolk.nl>
2022-08-17 23:05:59 +0200

    try this

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
-window.settings = JSON.parse(settings);
-
-const reWriteTld = tld => {
-  const allElements = Array.from(document.querySelectorAll(`[data-tld="true"]`));
-  allElements.forEach(element => {
-    console.log(element.innerText);
-  })
-};
-
 const handleKeyDown = e => {
   if (e.key === 'Enter') {
-    const url = `https://impervious.domains/name/${e.target.value}${window.settings.tld}/register/`;
+    const url = `https://impervious.domains/name/${e.target.value}${window.tld}/register/`;
     location.href = url;
   };
 };
 
 const loadedHandler = () => {
-  reWriteTld(window.settings.tld);
+  const allElements = Array.from(document.querySelectorAll(`[data-tld="true"]`));
+  allElements.forEach(element => {
+    const t = element.innerText;
+    const r = t.replace("{{tld}}",window.tld);
+    console.log(t,r);
+  });
 };
 
 (() => {
