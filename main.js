f279dad36f8015a0f94ef51f1882fe5e20df100b
Sh1Tt <info@mauricestolk.nl>
2022-08-18 20:50:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,57 @@
-(() => {
-  const options = {
-    color: [
-      'linear-gradient(7deg, transparent, black)',
-      'linear-gradient(7deg, blueviolet, purple)',
-      'linear-gradient(7deg, cyan, gold)',
-      'linear-gradient(7deg, rgb(100,80,146), rgb(200,160,255), rgba(100,27,245) 52%)',
-      'linear-gradient(205deg, goldenrod 2%, rgb(255,52,255), rgb(55,152,255))'
-    ]
+const options = {
+  color: [
+    'linear-gradient(7deg, transparent, black)',
+    'linear-gradient(7deg, blueviolet, purple)',
+    'linear-gradient(7deg, cyan, gold)',
+    'linear-gradient(7deg, rgb(100,80,146), rgb(200,160,255), rgba(100,27,245) 52%)',
+    'linear-gradient(205deg, goldenrod 2%, rgb(255,52,255), rgb(55,152,255))'
+  ]
+};
+
+let name;
+
+const card = document.body.querySelector(`#card`);
+const input = document.body.querySelector(`#domainname`);
+
+const showcase = document.body.querySelector(`[data-std="domainname"]`);
+
+function randomizeColor() {
+  const randomGradient = options.color[Math.floor(Math.random()*options.color.length)];
+  card.style.background = randomGradient;
+};
+
+function rewritePage() {
+  const allElements = Array.from(document.querySelectorAll(`[data-tld]`));
+  allElements.forEach(element => {
+    const t = element.innerText;
+    const r = t.replace("{{tld}}",window.tld);
+    element.innerText = r;
+  });
+};
+
+function keyDownHandler() {
+  if (event.key === 'Enter') {
+    const url = `https://impervious.domains/name/${event.target.value}${window.tld}/register/`;
+    location.target = '_blanc';
+    location.href = url;
+  };
+};
+
+function setDomain(n) {
+  showcase.innerText = n;
+};
+
+function updateHandler() {
+  if (input) {
+    console.log(input)
+    name = input.value;
   };
 
-  let name;
+  randomizeColor();
+}
 
-  const card = document.body.querySelector(`#card`);
-  const input = document.body.querySelector(`#domainname`);
-  
-  const showcase = document.body.querySelector(`[data-std="domainname"]`);
+function loadedHandler() {
+  rewritePage();
+};
 
-  function randomizeColor() {
-    const randomGradient = options.color[Math.floor(Math.random()*options.color.length)];
-    card.style.background = randomGradient;
-  };
-
-  function rewritePage() {
-    const allElements = Array.from(document.querySelectorAll(`[data-tld]`));
-    allElements.forEach(element => {
-      const t = element.innerText;
-      const r = t.replace("{{tld}}",window.tld);
-      element.innerText = r;
-    });
-  };
-
-  function keyDownHandler() {
-    if (event.key === 'Enter') {
-      const url = `https://impervious.domains/name/${event.target.value}${window.tld}/register/`;
-      location.target = '_blanc';
-      location.href = url;
-    };
-  };
-
-  function setDomain(n) {
-    showcase.innerText = n;
-  };
-
-  function updateHandler() {
-    if (input) {
-      console.log(input)
-      name = input.value;
-    };
-
-    randomizeColor();
-  }
-  
-  function loadedHandler() {
-    rewritePage();
-  };
-
-  document.addEventListener("DOMContentLoaded", loadedHandler, false);
-})();
+document.addEventListener("DOMContentLoaded", loadedHandler, false);
