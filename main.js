4e06c861e98526a43a9cdccdac72fd2bec4e176e
Sh1Tt <info@mauricestolk.nl>
2022-08-18 09:25:02 +0200

    Summ

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const color = [
+  'darkgrey',
+  'blueviolet',
+  'cyan',
+  'hotpink',
+  'goldenrod'
+];
+
 function keyDownHandler() {
   if (event.key === 'Enter') {
     const url = `https://impervious.domains/name/${event.target.value}${window.tld}/register/`;
@@ -6,7 +14,13 @@ function keyDownHandler() {
   };
 };
 
+function updateHandler() {
+  card.style.background = color[Math.floor(Math.random()*5)]
+}
+
 function loadedHandler() {
+  const card = document.body.querySelector(`#card`);
+
   const allElements = Array.from(document.querySelectorAll(`[data-tld="true"]`));
   allElements.forEach(element => {
     const t = element.innerText;
