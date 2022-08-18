f253fa47adbfc0ba05c003498bd233e48d667d86
Sh1Tt <info@mauricestolk.nl>
2022-08-18 20:19:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,48 +8,49 @@ window.options = {
   ]
 };
 
-const settings = window.options;
-
-function randomizeColor() {
-  const randomGradient = settings.color[Math.floor(Math.random()*settings.color.length)];
-  card.style.background = randomGradient;
-};
-
-function setTLD() {
-  const allElements = Array.from(document.querySelectorAll(`[data-tld]`));
-  allElements.forEach(element => {
-    const t = element.innerText;
-    const r = t.replace("{{tld}}",window.tld);
-    element.innerText = r;
-  });
-};
-
-function keyDownHandler() {
-  if (event.key === 'Enter') {
-    const url = `https://impervious.domains/name/${event.target.value}${window.tld}/register/`;
-    location.target = '_blanc';
-    location.href = url;
-  };
-};
-
-function setDomain(n) {
-  showcase.innerText = n;
-};
-
-function updateHandler() {
-  name = input.value;
-  setDomain(name);
-  randomizeColor();
-}
-
-function loadedHandler() {
-  const card = document.body.querySelector(`#card`);
-  const input = document.body.querySelector(`#domainname`);
-  const showcase = document.body.querySelector(`[data-std="domainname"]`);
-  setTLD();
-};
-
 (() => {
+  const settings = window.options;
+  
   let name;
+
+  function randomizeColor() {
+    const randomGradient = settings.color[Math.floor(Math.random()*settings.color.length)];
+    card.style.background = randomGradient;
+  };
+
+  function setTLD() {
+    const allElements = Array.from(document.querySelectorAll(`[data-tld]`));
+    allElements.forEach(element => {
+      const t = element.innerText;
+      const r = t.replace("{{tld}}",window.tld);
+      element.innerText = r;
+    });
+  };
+
+  function keyDownHandler() {
+    if (event.key === 'Enter') {
+      const url = `https://impervious.domains/name/${event.target.value}${window.tld}/register/`;
+      location.target = '_blanc';
+      location.href = url;
+    };
+  };
+
+  function setDomain(n) {
+    showcase.innerText = n;
+  };
+
+  function updateHandler() {
+    name = input.value;
+    setDomain(name);
+    randomizeColor();
+  }
+
+  function loadedHandler() {
+    const card = document.body.querySelector(`#card`);
+    const input = document.body.querySelector(`#domainname`);
+    const showcase = document.body.querySelector(`[data-std="domainname"]`);
+    setTLD();
+  };
+
   document.addEventListener("DOMContentLoaded", loadedHandler, false);
 })();
