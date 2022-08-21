c32ba040b56b44ef2474bb09559044c05bc440e9
Sh1Tt <info@mauricestolk.nl>
2022-08-21 15:09:37 +0200

    Sunday doodles

---
+++ README.md
@@ -0,0 +1,39 @@
+This project is a fork of [theshake/domains:master](https://replit.com/@theshake/domains).
+All credit is theirs.
+
+## Deploy your own marketplace
+## `1 - clone repo`
+```
+    clone this repo
+```
+
+## `2 - edit /settings.js`
+```
+    Edit the settings.js file to your flavor.  
+    Enter your TLD.  
+    The color array will be used by the randomized color picker, yo can add as many color
+    (gradient)s as you like.  
+    Set the background image that will partly show through the randomized colors. This can be any image, gif or nft.  
+    Last but not least, set your hnschat name for your support channel.  
+```
+
+## `3 - edit /theme.css`
+```
+    All colors are brought back to some css variables.  
+    Edit the variable and the color will change throughout the page.  
+      
+    #tip: Use VScode's built-in color picker and change the color-scheme in seconds!
+```
+
+## `4 - configure DNS`
+``` 
+    Create or edit the CNAME file to contain only your domain name. (E.g. `tld.sldmarket`).  
+    Go to your repo's settings - Pages and enter a Custom domain.
+    Edit your records on your nameserver to point to your github pages.
+```
+
+## `5 - sell domainnames`
+```
+    Congratulations! With your own marketplace up and running, al thats left to do is sell domains!  
+    Please have a beautiful time!
+```
---
+++ scripts/main.js
@@ -0,0 +1,62 @@
+const { tld, color, nft, support } = options = window.settings;
+
+const card = document.body.querySelector(`#card`);
+const input = document.body.querySelector(`#domainname`);
+const showcase = document.body.querySelector(`[data-std="domainname"]`);
+
+function randomizeColor() {
+  const randomGradient = options.color[Math.floor(Math.random()*options.color.length)];
+  card.classList.add("fader");
+  card.style.background = randomGradient;
+  setTimeout(() => {
+    card.classList.remove("fader");
+  }, 400);
+};
+
+function setDomain(n) {
+  showcase.innerText = n;
+};
+
+function updateHandler() {
+  const domain = event.target.value + tld;
+  setDomain(domain);
+  randomizeColor();
+};
+
+function rewritePage() {
+  const allElements = Array.from(document.querySelectorAll(`[data-tld]`));
+  const chatLink = `https://hns.chat/#message:${support.chatname}`;
+  allElements.forEach(element => {
+    const t = element.innerText;
+    const r = t.replace("{{tld}}",tld);
+    element.innerText = r;
+  });
+  document.body.querySelector(`#chat`).href = chatLink;
+};
+
+function loadNft() {
+  const image = document.body.querySelector(`#nft`);
+  const { url, alt, w, h } = nft;
+  image.alt = alt;
+  image.width = w;
+  image.height = h;
+  image.src = url;
+};
+
+function keydownHandler() {
+  if (event.key === 'Enter') {
+    const domain = event.target.value + tld;
+    const url = `https://impervious.domains/name/${domain}/register/`;
+    location.target = '_blanc';
+    location.href = url;
+  };
+};
+
+function loadedHandler() {
+  loadNft();
+  rewritePage();
+};
+
+document.addEventListener("DOMContentLoaded", 
+  loadedHandler
+, false);
