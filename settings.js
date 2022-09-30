a2c832b4f4799d52b77aa007d9c46c27582b43cb
Maurice Stolk <84513986+Sh1Tt@users.noreply.github.com>
2022-09-30 18:17:58 +0200

    Added gradients

--- settings.js
+++ settings.js
@@ -5,7 +5,9 @@ window.settings = {
 		'linear-gradient(43deg, blueviolet, purple)',
 		'linear-gradient(7deg, cyan, gold)',
 		'linear-gradient(7deg, rgb(100,80,146), rgb(200,160,255), rgba(100,27,245) 52%)',
-		'linear-gradient(205deg, goldenrod 2%, rgb(255,52,255), rgb(55,152,255))'
+		'linear-gradient(205deg, goldenrod 2%, rgb(255,52,255), rgb(55,152,255))',
+		'conic-gradient(rgba(20,23,33,.5), rgba(195,23,33,.5), rgba(20,23,33,.5))',
+		'repeating-linear-gradient(-3deg, rgba(27,127,200,.5), rgba(72,227,27,.5));'
 	],
 	nft: {
 		url: "https://mauricestolk.nl/api/registry/b7fe8a8d61.gif",
